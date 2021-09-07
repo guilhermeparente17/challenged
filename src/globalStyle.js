ffcb83e137cddf595caed080281aab9ba10aa474
Guilherme <guilherme.parente17@outlook.com>
2021-09-07 13:53:37 -0300

    Instalando o Styled-Components, criando component Header e pagina Homepage

---
+++ src/globalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap');
+
+    *{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+        font-family: 'Open Sans', sans-serif;
+    }
+`
+
+export default GlobalStyle
