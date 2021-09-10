9f44f46e063b635190784280394119d69baacb42
Guilherme <guilherme.parente17@outlook.com>
2021-09-09 23:48:41 -0300

    teste

--- src/components/MainPage/Mainpage.jsx
+++ src/components/MainPage/Mainpage.jsx
@@ -12,7 +12,7 @@ const Mainpage = () => {
     return (
         <div>
             <MainContainer>
-                <Form />
+                
             </MainContainer>
             <MainFooter>
 
